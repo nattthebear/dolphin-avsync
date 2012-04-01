4cb36342e67832faec26038c53e00cfaee191a83
natt <goyuken@gmail.com>
2012-04-01 00:32:15 -0400

    fix bug where first frame of a new segment (2GiB split) (CFR mode) could be wrong

--- Source/Core/VideoCommon/Src/AVIDump.cpp
+++ Source/Core/VideoCommon/Src/AVIDump.cpp
@@ -77,6 +77,12 @@ bool AVIDump::Start(HWND hWnd, int w, int h)
 			return false;
 		std::fprintf (timecodes, "# timecode format v2\n");
 	}
+
+	// clear CFR frame cache on start, not on file create (which is also segment switch)
+	SetBitmapFormat ();
+	if (ac_Config.m_DumpAudioToAVI)
+		StoreFrame (NULL);
+
 	return CreateFile();
 }
 
@@ -113,9 +119,6 @@ bool AVIDump::CreateFile()
 	}
 
 	SetBitmapFormat();
-	// prep a null stored frame (CFR mode)
-	if (ac_Config.m_DumpAudioToAVI)
-		StoreFrame (NULL);
 
 	NOTICE_LOG(VIDEO, "Setting video format...");
 	if (!SetVideoFormat())
@@ -346,7 +349,7 @@ void AVIDump::AddSound (const short *data, int nsamp, int rate)
 // the CFR dump design doesn't let you dump until you know the NEXT timecode.
 // so we have to save a frame and always be behind
 
-void *storedframe;
+void *storedframe = 0;
 unsigned storedframesz = 0;
 
 
@@ -356,6 +359,7 @@ void AVIDump::StoreFrame (const void *data)
 	{
 		storedframe = realloc (storedframe, m_bitmap.biSizeImage);
 		storedframesz = m_bitmap.biSizeImage;
+		memset (storedframe, 0, m_bitmap.biSizeImage);
 	}
 	if (storedframe)
 	{
